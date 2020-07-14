b07f206a4708925bb6c0753757a69f8854798960
Sanjay Sanjel <sanjay.sanjel018@gmail.com>
2020-07-14 23:55:28 +0545

    Updates

---
+++ precache-manifest.0c2d90cea1b81c653a5bfeb3750d6fbb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2cc0212f3b403f779a9c3314daa6945f",
+    "url": "/sanjaysanjel019/test-monster/index.html"
+  },
+  {
+    "revision": "0dc2c9cbda458903b630",
+    "url": "/sanjaysanjel019/test-monster/static/css/main.ee93d3c9.chunk.css"
+  },
+  {
+    "revision": "82815bfcb9bf73d8a971",
+    "url": "/sanjaysanjel019/test-monster/static/js/2.be6af004.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/sanjaysanjel019/test-monster/static/js/2.be6af004.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0dc2c9cbda458903b630",
+    "url": "/sanjaysanjel019/test-monster/static/js/main.e35104a6.chunk.js"
+  },
+  {
+    "revision": "cead2a18b192c4a9e1aa",
+    "url": "/sanjaysanjel019/test-monster/static/js/runtime-main.893b8c30.js"
+  }
+]);
